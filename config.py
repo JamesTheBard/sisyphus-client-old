f3decc64697b115311de422f9673056e6b0b1a10
Jason Weatherly <jamesthebard@gmail.com>
2021-03-25 21:44:21 -0400

    Cleaned up the Config file

--- config.py
+++ config.py
@@ -14,16 +14,16 @@ class Config:
     REDIS_QUEUE_NAME = 'queue'
     REDIS_COMPLETED_LIST = 'completed'
 
+    UUID_NAMESPACE = uuid.UUID('0067f190-fa1e-461f-9115-6193c804c883')
+    HOSTNAME = os.getenv('HOSTNAME_OVERRIDE', platform.node())
+    HOST_UUID = str(uuid.uuid5(UUID_NAMESPACE, HOSTNAME))
+
     # Ffmpeg Module Options
     # FFMPEG_BIN_PATH = "/usr/bin/ffmpeg"
     FFMPEG_MONGO_DB = "profiles"
     FFMPEG_MONGO_COLLECTION = "encoding"
     FFMPEG_MONGO_URI = os.environ.get("MONGO_URI", "mongodb://root:root@localhost:27017/profiles?authSource=admin")
 
-    UUID_NAMESPACE = uuid.UUID('0067f190-fa1e-461f-9115-6193c804c883')
-    HOSTNAME = os.getenv('HOSTNAME_OVERRIDE', platform.node())
-    HOST_UUID = str(uuid.uuid5(UUID_NAMESPACE, HOSTNAME))
-
+    # Mkvmerge Module Options
     MKVMERGE_ENABLE_FONT_ATTACHMENTS = True
     MKVMERGE_FONT_DIRECTORY = Path(os.getenv('FONT_DIRECTORY', '/mnt/phoenix/Server/fonts'))
-
